bcc9988664ec8984ef204845914bcb43a4dfe967
Mahesa Fadhillah Andre <167867383+mahesa005@users.noreply.github.com>
2024-08-25 22:22:00 +0700

    Update-2-Study-Space-Page

--- frontend/src/Components/Box.jsx
+++ frontend/src/Components/Box.jsx
@@ -1,4 +1,13 @@
-import '../App.css';
 import './Styles.css';
 
+const Box = ({ placeName, openingHours, imageURL }) => (
+    <button className="box-button">
+        <img src={imageURL} alt={placeName} className="box-image" />
+        <div className="box-content">
+            <div className="box-name">{placeName}</div>
+            <div className="box-hours">{openingHours}</div>
+        </div>
+    </button>
+);
 
+export default Box;
--- frontend/src/Pages/Study-Space/StudySpace.jsx
+++ frontend/src/Pages/Study-Space/StudySpace.jsx
@@ -2,6 +2,7 @@ import Sidebar from "../../Components/Sidebar";
 import Topbar from "../../Components/Topbar";
 import Bubble from "../../Components/Bubble";
 import '../../Components/Styles.css'
+import Box from "../../Components/Box";
 function StudySpace() {
   return (
     <div className="containerSS">
@@ -13,6 +14,21 @@ function StudySpace() {
                     <Bubble placeholderText="near ITB Ganesha"/>
                     <Bubble placeholderText="near ITB Jatinangor"/>
                     <Bubble placeholderText="near ITB Cirebon"/>
+                </div>
+                <div className="boxSS">
+                    <Box 
+                    placeName={"Koica"} 
+                    openingHours={"06:00-16:00"} 
+                    imageUrl={"/koica.jpg"}/>
+                    <Box 
+                    placeName={"Koica"} 
+                    openingHours={"06:00-16:00"} 
+                    imageUrl={"../public/logo512.png"}/>
+                    <Box 
+                    placeName={"Koica"} 
+                    openingHours={"06:00-16:00"} 
+                    imageUrl={"../public/logo512.png"}/>
+                    
                 </div>
             </div>
         </div>
