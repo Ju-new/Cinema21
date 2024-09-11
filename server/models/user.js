beefba1f54c6812632ca77b3af66a7b514131e0e
Arthur <juliusarthur2005@gmail.com>
2024-09-11 23:17:47 +0700

    user model

---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/LoginForm")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema1=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection1=new mongoose.model('LogInCollection1',logInSchema1)
+
+module.exports=LogInCollection1
