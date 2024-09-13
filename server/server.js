654143020b7107ccb869bfad015742497d35f6f1
Arthur <juliusarthur2005@gmail.com>
2024-09-13 18:17:31 +0700

    add auth

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 const dbConnect = require("./dbConnect");
 const placeRoutes = require("./route/places");
+const userRoutes = require("./route/user");
 
 dbConnect();
 
@@ -11,6 +12,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api",placeRoutes);
+app.use("/api/auth",userRoutes);
+
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
